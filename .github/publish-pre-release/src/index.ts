79fe08707df44178ecaf826197eb0bc385030336
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-10-15 16:48:11 -0400

    move inputs to env

--- .github/publish-pre-release/src/index.ts
+++ .github/publish-pre-release/src/index.ts
@@ -1,20 +1,27 @@
-import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { createRelease } from './functions';
 
 async function run() {
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const TAG = process.env.TAG;
+  const CHANGELOG_DIFFERENCES = process.env.CHANGELOG;
 
   if (!GITHUB_TOKEN) {
     throw new Error('Github token missing in action');
   }
 
+  if (!TAG) {
+    throw new Error('missing new tag version');
+  }
+
+  if (!CHANGELOG_DIFFERENCES) {
+    throw new Error('missing changelog');
+  }
+
   const octoKit = github.getOctokit(GITHUB_TOKEN);
   const context = github.context;
-  const tag = core.getInput('tag', { required: true });
-  const changelogDifferences = core.getInput('changelog', { required: true });
 
-  await createRelease(context, octoKit, tag, changelogDifferences);
+  await createRelease(context, octoKit, TAG, CHANGELOG_DIFFERENCES);
 }
 
 run();
