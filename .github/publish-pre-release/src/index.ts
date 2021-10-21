4ac8af64d4f7da350aacf7d0017acdbb8a388f84
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-10-21 13:46:58 -0400

    test octokit request

--- .github/publish-pre-release/src/index.ts
+++ .github/publish-pre-release/src/index.ts
@@ -4,7 +4,7 @@ import { createRelease } from './functions';
 async function run() {
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
   const TAG = process.env.TAG;
-  const CHANGELOG_DIFFERENCES = process.env.CHANGELOG;
+  // const CHANGELOG_DIFFERENCES = process.env.CHANGELOG;
 
   if (!GITHUB_TOKEN) {
     throw new Error('Github token missing in action');
@@ -14,14 +14,25 @@ async function run() {
     throw new Error('missing new tag version');
   }
 
-  if (!CHANGELOG_DIFFERENCES) {
-    throw new Error('missing changelog');
-  }
+  // if (!CHANGELOG_DIFFERENCES) {
+  //   throw new Error('missing changelog');
+  // }
 
   const octoKit = github.getOctokit(GITHUB_TOKEN);
   const context = github.context;
 
-  await createRelease(context, octoKit, TAG, CHANGELOG_DIFFERENCES);
+  const whoamlol = await octoKit.request(
+    'GET /repos/{owner}/{repo}/pulls/{pull_number}',
+    {
+      owner: 'bgambocjaviniar',
+      repo: 'spartacus',
+      pull_number: 13,
+    }
+  );
+
+  console.log('colio', whoamlol);
+
+  await createRelease(context, octoKit, TAG, 'test');
 }
 
 run();
