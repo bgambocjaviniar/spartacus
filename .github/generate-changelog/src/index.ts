d3be37cd4fc3a9b6b5930ebc6ef3e02b2fc1030c
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-11-08 00:02:21 -0500

    test3

--- .github/generate-changelog/src/index.ts
+++ .github/generate-changelog/src/index.ts
@@ -2,9 +2,19 @@ import * as github from '@actions/github';
 
 async function run() {
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const FROM_TAG = process.env.FROM_TAG;
+  const TO_TAG = process.env.TO_TAG;
 
   if (!GITHUB_TOKEN) {
     throw new Error('Github token missing in action');
+  }
+
+  if (!FROM_TAG) {
+    throw new Error('From tag is missing');
+  }
+
+  if (!TO_TAG) {
+    throw new Error('To tag is missing');
   }
 
   //   const octoKit = github.getOctokit(GITHUB_TOKEN);
@@ -13,7 +23,8 @@ async function run() {
   if (!context.payload.pull_request) {
     throw new Error('Not triggered by a pull request');
   }
-
+  console.log(FROM_TAG);
+  console.log(TO_TAG);
   console.log(context.payload.pull_request.base.ref);
 }
 
