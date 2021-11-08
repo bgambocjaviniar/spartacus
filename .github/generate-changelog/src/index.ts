4c15c7b2ab3eaaf86e15a3b813710e52fc05e690
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-11-08 03:11:34 -0500

    test

--- .github/generate-changelog/src/index.ts
+++ .github/generate-changelog/src/index.ts
@@ -2,24 +2,28 @@ import * as core from '@actions/core';
 
 async function run() {
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-  const FROM_TAG = process.env.FROM_TAG;
-  const TO_TAG = process.env.TO_TAG;
+  // const FROM_TAG = process.env.FROM_TAG;
+  // const TO_TAG = process.env.TO_TAG;
 
   if (!GITHUB_TOKEN) {
     throw new Error('Github token missing in action');
   }
 
-  if (!FROM_TAG) {
-    throw new Error('From tag is missing');
-  }
+  // if (!FROM_TAG) {
+  //   throw new Error('From tag is missing');
+  // }
 
-  if (!TO_TAG) {
-    throw new Error('To tag is missing');
-  }
+  // if (!TO_TAG) {
+  //   throw new Error('To tag is missing');
+  // }
 
   //   const octoKit = github.getOctokit(GITHUB_TOKEN);
   // const context = github.context;
 
+  // if they don't exist 'tags' for to and from, then git describe for head and previous head. test for now
+
+  core.setOutput('fromTag', '3.0.0-test');
+  core.setOutput('toTag', '5.0.0-test');
   core.setOutput('changelog', 'coolio');
 }
 
