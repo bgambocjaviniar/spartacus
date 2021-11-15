a2a1fffd3f2e245a30ab692a9fc67fd060147201
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2021-11-14 23:23:54 -0500

    what's inside the changelog script

--- testbash.sh
+++ testbash.sh
@@ -2,11 +2,14 @@
 set -e
 
 please=$(npx ts-node scripts/changelog.ts --verbose --from asm-4.0.0)
+wow=$(cat scripts/changelog.ts)
+
+echo "$wow"
 
 echo "cool"
 
-echo "wow $please"
+# echo "wow $please"
 
-echo "did it work"
+# echo "did it work"
 
-echo "$please"
+# echo "$please"
