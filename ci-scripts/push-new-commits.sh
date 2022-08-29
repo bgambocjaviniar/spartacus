7313bc1491064a6521d7e7df8d2c24612e908b0d
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-08-29 03:53:41 -0400

    push to wow

--- ci-scripts/push-new-commits.sh
+++ ci-scripts/push-new-commits.sh
@@ -6,6 +6,6 @@ COMMITS_SHA=`git rev-list $GITHUB_SHA..HEAD | sort -r`
 
 
 for COMMIT_SHA in $COMMITS_SHA; do
-    git push ${{secrets.PWA_CX_COMMERCE}} $COMMIT_SHA:develop -f
+    git push ${{secrets.PWA_CX_COMMERCE}} $COMMIT_SHA:wow -f
 done
 
