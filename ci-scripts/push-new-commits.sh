6b28ef1c74865c6ae9fd6cd20b0910c0ae0fdd34
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-08-29 04:01:34 -0400

    vat

--- ci-scripts/push-new-commits.sh
+++ ci-scripts/push-new-commits.sh
@@ -7,6 +7,6 @@ COMMITS_SHA=`git rev-list $GITHUB_SHA..HEAD | sort -r`
 
 for COMMIT_SHA in $COMMITS_SHA; do
     echo $COMMIT_SHA
-    `git push $WOW $COMMIT_SHA:wow -f`
+    git push $WOW $COMMIT_SHA:develop -f
 done
 
