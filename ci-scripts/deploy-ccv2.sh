626aa614af4d9568c76ac63efa01dc0adfd4d6e9
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-09-12 23:49:53 -0400

    test

--- ci-scripts/deploy-ccv2.sh
+++ ci-scripts/deploy-ccv2.sh
@@ -1,55 +1,76 @@
 #!/usr/bin/env bash
 
+B2C_STORE="spartacusstore"
+B2B_STORE="b2bspastore"
+
+# echo "-----"
+# echo "Verify source branch exist"
+
+# IS_BRANCH=`git ls-remote --heads origin $SOURCE_BRANCH_TO_DEPLOY`
+
+# if [ -z "$IS_BRANCH" ]; then
+#     echo "Error downloading $SOURCE_BRANCH_TO_DEPLOY zip/tar. Verify branch name exist on the public Spartacus repository"
+#     exit 1
+# fi
+
+# echo "-----"
+# echo "Verify CCv2 branch exist"
+
+# IS_BRANCH=`git ls-remote --heads https://${GHT_USER}:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
+
+# if [ -z "$IS_BRANCH" ]; then
+#     echo "Error downloading $CCV2_BRANCH zip/tar. Verify branch name exist on the ccv2 repository"
+#     exit 1
+# fi
+
+# echo "-----"
+# echo "Comment out occBaseUrl from configration to allow index.html meta tag to set the occBaseUrl"
+
+# sed -i 's/baseUrl: environment.occBaseUrl/\/\/ baseUrl: environment.occBaseUrl/gi' projects/storefrontapp/src/app/app.module.ts
+
+# echo "-----"
+# echo "Verify app.module.ts has occBaseUrl commented"
+
+# cat projects/storefrontapp/src/app/app.module.ts
+
+# if grep -Fq "// baseUrl: environment.occBaseUrl" projects/storefrontapp/src/app/app.module.ts
+# then
+#     echo "Base url has been successfully commented out from app.module.ts"
+# else
+#     echo "Base url is not commented out from app.module.ts"
+#     exit 1
+# fi
+
+
+# echo "-----"
+# echo "Build Spartacus libraries"
+# yarn build:libs
+
+# echo "-----"
+# echo "Build SSR"
+
+# # to change
+# yarn build:ssr:ci
+
+# echo "-----"
+# echo "Build CSR"
+
+# yarn build
+
 echo "-----"
-echo "Verify source branch exist"
+echo "Clone ccv2 repository"
 
-IS_BRANCH=`git ls-remote --heads origin $SOURCE_BRANCH_TO_DEPLOY`
-
-if [ -z "$IS_BRANCH" ]; then
-    echo "Error downloading $SOURCE_BRANCH_TO_DEPLOY zip/tar. Verify branch name exist on the public Spartacus repository"
-    exit 1
-fi
+git clone -b spa_p4_dist_test "https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/$GHT_REPO.git"
 
 echo "-----"
-echo "Verify CCv2 branch exist"
+echo "Deleting  ccv2 repository"
 
-IS_BRANCH=`git ls-remote --heads https://${GHT_USER}:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
-
-if [ -z "$IS_BRANCH" ]; then
-    echo "Error downloading $CCV2_BRANCH zip/tar. Verify branch name exist on the ccv2 repository"
-    exit 1
-fi
-
-echo "-----"
-echo "Comment out occBaseUrl from configration to allow index.html meta tag to set the occBaseUrl"
-
-sed -i 's/baseUrl: environment.occBaseUrl/\/\/ baseUrl: environment.occBaseUrl/gi' projects/storefrontapp/src/app/app.module.ts
-
-echo "-----"
-echo "Verify app.module.ts has occBaseUrl commented"
-
-cat projects/storefrontapp/src/app/app.module.ts
-
-if grep -Fq "// baseUrl: environment.occBaseUrl" projects/storefrontapp/src/app/app.module.ts
-then
-    echo "app.module.ts has successfully commented out baseUrl"
-else
-    echo "Base url is not commented out from app.module.ts"
-    exit 1
-fi
+cd "$GH_REPO/js-storefront"
+rm -rf $B2C_STORE
+rm -rf $B2B_STORE
 
 
 echo "-----"
-echo "Build Spartacus libraries"
-yarn build:libs
+echo "Deleting  ccv2 repository"
 
-echo "-----"
-echo "Build SSR"
-
-# to change
-yarn build:ssr:ci
-
-echo "-----"
-echo "Build CSR"
-
-yarn build
+ls -al
