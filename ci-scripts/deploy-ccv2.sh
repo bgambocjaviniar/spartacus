0af197d95f862f1f5d86f7eb17dac353c1b7c9d6
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-09-13 00:17:27 -0400

    test

--- ci-scripts/deploy-ccv2.sh
+++ ci-scripts/deploy-ccv2.sh
@@ -4,43 +4,43 @@ set -xe
 B2C_STORE="spartacusstore"
 B2B_STORE="b2bspastore"
 
-# echo "-----"
-# echo "Verify source branch exist"
+echo "-----"
+echo "Verify source branch exist"
 
-# IS_BRANCH=`git ls-remote --heads origin $SOURCE_BRANCH_TO_DEPLOY`
+IS_BRANCH=`git ls-remote --heads origin $SOURCE_BRANCH_TO_DEPLOY`
 
-# if [ -z "$IS_BRANCH" ]; then
-#     echo "Error downloading $SOURCE_BRANCH_TO_DEPLOY zip/tar. Verify branch name exist on the public Spartacus repository"
-#     exit 1
-# fi
+if [ -z "$IS_BRANCH" ]; then
+    echo "Error downloading $SOURCE_BRANCH_TO_DEPLOY zip/tar. Verify branch name exist on the public Spartacus repository"
+    exit 1
+fi
 
-# echo "-----"
-# echo "Verify CCv2 branch exist"
+echo "-----"
+echo "Verify CCv2 branch exist"
 
-# IS_BRANCH=`git ls-remote --heads https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
+IS_BRANCH=`git ls-remote --heads https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce/ccv2-ec-dev-project-for-tests.git $CCV2_BRANCH`
 
-# if [ -z "$IS_BRANCH" ]; then
-#     echo "Error downloading $CCV2_BRANCH zip/tar. Verify branch name exist on the ccv2 repository"
-#     exit 1
-# fi
+if [ -z "$IS_BRANCH" ]; then
+    echo "Error downloading $CCV2_BRANCH zip/tar. Verify branch name exist on the ccv2 repository"
+    exit 1
+fi
 
-# echo "-----"
-# echo "Comment out occBaseUrl from configration to allow index.html meta tag to set the occBaseUrl"
+echo "-----"
+echo "Comment out occBaseUrl from configration to allow index.html meta tag to set the occBaseUrl"
 
-# sed -i 's/baseUrl: environment.occBaseUrl/\/\/ baseUrl: environment.occBaseUrl/gi' projects/storefrontapp/src/app/app.module.ts
+sed -i 's/baseUrl: environment.occBaseUrl/\/\/ baseUrl: environment.occBaseUrl/gi' projects/storefrontapp/src/app/app.module.ts
 
-# echo "-----"
-# echo "Verify app.module.ts has occBaseUrl commented"
+echo "-----"
+echo "Verify app.module.ts has occBaseUrl commented"
 
-# cat projects/storefrontapp/src/app/app.module.ts
+cat projects/storefrontapp/src/app/app.module.ts
 
-# if grep -Fq "// baseUrl: environment.occBaseUrl" projects/storefrontapp/src/app/app.module.ts
-# then
-#     echo "Base url has been successfully commented out from app.module.ts"
-# else
-#     echo "Base url is not commented out from app.module.ts"
-#     exit 1
-# fi
+if grep -Fq "// baseUrl: environment.occBaseUrl" projects/storefrontapp/src/app/app.module.ts
+then
+    echo "Base url has been successfully commented out from app.module.ts"
+else
+    echo "Base url is not commented out from app.module.ts"
+    exit 1
+fi
 
 
 # echo "-----"
@@ -65,14 +65,17 @@ git clone -b spa_p4_dist_test https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.t
 ls -ls
 
 echo "-----"
-echo "Deleting  ccv2 repository"
+echo "Updating js-storefront to adhere to the ccv2 dist strucutre"
 
 cd "$GHT_REPO/js-storefront"
 rm -rf $B2C_STORE
 rm -rf $B2B_STORE
 
+mkdir -p $B2C_STORE/dist/$B2C_STORE
+mkdir -p $B2B_STORE/dist/$B2B_STORE
 
-echo "-----"
-echo "Deleting  ccv2 repository"
+ls -al
+
+cd -
 
 ls -al
