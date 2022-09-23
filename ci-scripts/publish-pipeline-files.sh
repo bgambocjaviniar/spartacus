bf2a9ccf6266f4dde41a4f7805fde3912339427a
Brian Gamboc-Javiniar <brian_javiniar@hotmail.com>
2022-09-23 11:37:09 -0400

    test

---
+++ ci-scripts/publish-pipeline-files.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+
+echo "-----"
+echo "Clone repo"
+
+SONAR_PATH="./sonar-project.properties"
+
+git clone -b $BRANCH_TO_SYNC https://$GHT_USER:$GHT_PRIVATE_REPO_TOKEN@github.tools.sap/cx-commerce-storefronts/$GHT_SPARTACUS_REPO.git
+cd $GHT_SPARTACUS_REPO
+
+# git pull
+
+git config --global user.email $GHT_EMAIL
+git config --global user.name $GHT_USER
+
+echo "-----"
+echo "Get files other branch"
+
+git checkout origin/sap-pipeline-files -- .
+
+echo "-----"
+echo "Configure sonar"
+
+sed -i "s%sonar.branch.name=%sonar.branch.name=$BRANCH_TO_SYNC@gi" $SONAR_PATH
+
+echo "---------------------------------------------------------------------------------------------------------------------------"
+echo "Verify app.module.ts has occBaseUrl commented"
+
+cat projects/storefrontapp/src/app/app.module.ts
+
+if grep -Fq "sonar.branch.name=$BRANCH_TO_SYNC" $SONAR_PATH
+then
+    echo "Branch value has successfully been appended to the sonar.branch.name"
+else
+    echo "Branch value has NOT been appended to the sonar.branch.name"
+    exit 1
+fi
+
+echo "-----"
+echo "Include files to synced branch"
+
+git commit -m "include remaining pipeline files"
+git push origin $BRANCH_TO_SYNC
