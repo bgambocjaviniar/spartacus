2b3a8be0a608e16493f3dac3ee9aa9657706526e
I506207 <brian.gamboc-javiniar@sap.com>
2022-10-04 21:58:01 -0400

    test

--- core-libs/setup/test.ts
+++ core-libs/setup/test.ts
@@ -1,20 +1,12 @@
-/*
- * SPDX-FileCopyrightText: 2022 SAP Spartacus team <spartacus-team@sap.com>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 // Zone.js and zone.js/testing should be imported as FIRST and in this ORDER:
-import 'zone.js';
-import 'zone.js/testing';
-
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
+  platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js';
+import 'zone.js/testing';
+
 
 declare const require: {
   context(
