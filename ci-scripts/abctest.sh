35118715414b3ca1397f05d8da429837518a0ec5
I506207 <brian.gamboc-javiniar@sap.com>
2023-05-01 03:46:28 -0400

    test11

---
+++ ci-scripts/abctest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+
+EXCLUDE_LIBS_AND_APPS=storefrontapp,setup,assets,core,schematics,storefrontlib,storefrontstyles,eslint-rules
+EXCLUDE_INTEGRATION_LIBS=cdc,cds,digital-payments,epd-visualization,s4om
+
+AFFECTED_E2ES=`npx nx print-affected --base=develop-test-3 --head=develop-test-4 --exclude=$EXCLUDE_LIBS_AND_APPS,$EXCLUDE_INTEGRATION_LIBS --select=projects | sed 's/, /,/g'`
+
+echo $AFFECTED_E2ES
